251fb933108f28f777c534e55a18fa989b6e5ff2
GusGus27 <g.ucedayacila@gmail.com>
2025-03-06 13:00:48 -0500

    Add files via upload

--- viewData.py
+++ viewData.py
@@ -7,7 +7,32 @@ print("Cargando datos de los participantes...")
 
 VentanasTotales = 0
 
-for participante in participantes:
+with open(f'data/WESAD/S2/S2.pkl', 'rb') as file:
+        data_loaded = pickle.load(file, encoding='latin1')
+
+labels = data_loaded['label']  # (4255300,)
+chest_signals = data_loaded['signal']['chest']  # Diccionario con las señales
+
+# Crear una máscara para filtrar solo los datos con label 1, 2 o 3
+valid_labels = {1, 2, 3}
+mask = np.isin(labels, list(valid_labels))  # Array booleano con True en los índices a conservar
+
+# Aplicar la máscara a las señales y a las etiquetas
+filtered_labels = labels[mask]  # Filtrar etiquetas
+
+filtered_signals = {}  # Diccionario para guardar señales filtradas
+for key in chest_signals.keys():
+    filtered_signals[key] = chest_signals[key][mask]  # Aplicar la máscara a cada señal
+
+# Verificar los tamaños después del filtrado
+print("Tamaño de las etiquetas filtradas:", filtered_labels.shape)
+for key, signal in filtered_signals.items():
+    print(f"Tamaño de {key} después del filtrado:", signal.shape)
+
+print(filtered_signals['ECG'].shape)
+
+
+'''for participante in participantes:
 
     # Cargar el objeto desde el archivo .pkl
     with open(f'data/WESAD/S{participante}/S{participante}.pkl', 'rb') as file:
@@ -43,7 +68,7 @@ for participante in participantes:
 
         total_valid = total_valid + count_ones
 
-        valid_windows = (count_ones - 21000)/350 + 1
+        valid_windows = (count_ones - 21000)/525 + 1
         windowTotal = windowTotal + valid_windows
         print(f"Cantidad de datos iguales a {estado} en 'label': {count_ones}")
         print(f"Ventanas disponibles: {valid_windows}")
@@ -53,4 +78,4 @@ for participante in participantes:
     print(f"Total de datos en validos: {windowTotal}")
     VentanasTotales = VentanasTotales + windowTotal
 
-print(f"Total de datos en validos: {VentanasTotales}")
+print(f"Total de datos en validos: {VentanasTotales}")'''
